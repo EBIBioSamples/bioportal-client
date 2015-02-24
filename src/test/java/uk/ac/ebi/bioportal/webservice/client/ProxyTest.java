2b62ed6a4b2d596e08fdb44ad12c983765a854e6
Marco Brandizi <brandizi@ebi.ac.uk>
2015-02-24 17:54:05 +0000

    Testing problems with Bamboo and proxy.

---
+++ src/test/java/uk/ac/ebi/bioportal/webservice/client/ProxyTest.java
@@ -0,0 +1,37 @@
+package uk.ac.ebi.bioportal.webservice.client;
+
+import static java.lang.System.out;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+
+import uk.ac.ebi.utils.io.IOUtils;
+
+/**
+ * TODO: comment me!
+ *
+ * @author brandizi
+ * <dl><dt>Date:</dt><dd>24 Feb 2015</dd>
+ *
+ */
+public class ProxyTest
+{
+	@Test
+	public void testProxy () throws MalformedURLException, IOException
+	{
+		out.println ( "------- http.proxyHost = " + System.getProperty ( "http.proxyHost" ) );
+		out.println ( "------- http.proxyPort = " + System.getProperty ( "http.proxyPort" ) );
+		
+		out.println ( "------- From Remote ------" );
+		out.println (
+			IOUtils.readInputFully ( 
+				new InputStreamReader ( new URL ( "http://www.ebi.ac.uk" ).openStream () ) 
+			).substring ( 0, 200 ) 
+		);
+		out.println ( "------- ------\n\n" );
+	}
+}
